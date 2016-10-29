07f6d811555e31b67cfbdfade3212b7e18228e0a
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2016-10-29 17:56:56 +0100

    Cp (Ex.4a) - now uses catch/FileNotFoundException to check if source file exists

--- src/Cp.java
+++ src/Cp.java
@@ -11,27 +11,25 @@ public class Cp {
 
     private void launch(String[] filenames) {
         File sourceFile = new File(filenames[0]);
-        if (!sourceFile.exists()) {
-            System.out.println(filenames[0] + " does not exist");
-        } else {
-            File destinationFile = new File(filenames[1]);
-            if (destinationFile.exists()) {
-                System.out.print("Overwrite " + destinationFile + " ? (y/n): ");
-                Scanner input = new Scanner(System.in);
-                if (!input.next().equals("y")) {
-                    System.out.println("OK - " + destinationFile + " won't be overwritten");
-                    return;
-                }
+        File destinationFile = new File(filenames[1]);
+        if (destinationFile.exists()) {
+            System.out.print("Overwrite " + destinationFile + " ? (y/n): ");
+            Scanner input = new Scanner(System.in);
+            if (!input.next().equals("y")) {
+                System.out.println("OK - " + destinationFile + " won't be overwritten");
+                return;
             }
-            try (BufferedReader in = new BufferedReader(new FileReader(sourceFile));
-                 PrintWriter out = new PrintWriter(destinationFile)) {
-                String line;
-                while ((line = in.readLine()) != null) {
-                    out.println(line);
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
+        }
+        try (BufferedReader in = new BufferedReader(new FileReader(sourceFile));
+             PrintWriter out = new PrintWriter(destinationFile)) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                out.println(line);
             }
+        } catch (FileNotFoundException ex) {
+            System.out.println(filenames[0] + " not found");
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
