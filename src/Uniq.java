964a352aaaaf66e109e9898357b69bf1237decbf
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2016-10-31 17:01:18 +0000

    Uniq (Ex.7) implemented - removes duplicate lines

---
+++ src/Uniq.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by chris on 31/10/2016.
+ */
+public class Uniq {
+    public static void main(String[] args) {
+        new Uniq().launch(args[0]);
+    }
+
+    private void launch(String filename) {
+        try (BufferedReader in = new BufferedReader(new FileReader(new File(filename)))) {
+            String line;
+            Set<String> lines = new LinkedHashSet<>();
+            while ((line = in.readLine()) != null) {
+                lines.add(line);
+            }
+            lines.forEach(System.out::println);
+        } catch (FileNotFoundException ex) {
+            System.out.println("File " + filename + " does not exist");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
