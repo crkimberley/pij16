39515cc55abab13208575ee181c5fde433fb53af
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2016-10-31 15:50:18 +0000

    Cp - implemented for copying single (Ex.4a) & multiple (Ex.4b) files

--- src/CpMultipleFiles.java
+++ src/CpMultipleFiles.java
@@ -4,11 +4,11 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * Created by chris on 28/10/2016.
+ * Created by chris on 31/10/2016.
  */
-/*public class Cp {
+public class CpMultipleFiles {
     public static void main(String[] args) {
-        new Cp().launch(args);
+        new CpMultipleFiles().launch(args);
     }
 
     private void launch(String[] filenames) {
@@ -21,29 +21,34 @@ import java.util.Scanner;
                 System.out.println(filename + " doesn't exist");
             }
         }
-        File directory = new File(files.get(files.size() - 1);
+        File directory = files.get(files.size() - 1);
         if (!directory.isDirectory()) {
-            System.out.println(directory.getName() + " isn't a directory");
+            System.out.println(directory + " isn't a directory");
             return;
         }
-        System.out.print("Overwrite directory " + directory.getName() + " ? (y/n): ");
+        System.out.print("Overwrite directory " + directory + " ? (y/n): ");
         Scanner input = new Scanner(System.in);
         if (!input.next().equals("y")) {
-            System.out.println(directory.getName() + " won't be overwritten");
+            System.out.println(directory + " won't be overwritten");
             return;
         }
-        for ()
-        try (BufferedReader in = new BufferedReader(new FileReader(files[]));
-             PrintWriter out = new PrintWriter(files.get(files.size() - 1))) {
+        for (int i=0; i<files.size()-1; i++) {
+            String pathname = directory + "/" + files.get(i);
+            copy(files.get(i), new File(pathname));
+        }
+    }
+
+    private void copy(File fileToCopy, File fileToCopyInto) {
+        try (BufferedReader in = new BufferedReader(new FileReader(fileToCopy));
+             PrintWriter out = new PrintWriter(fileToCopyInto)) {
             String line;
             while ((line = in.readLine()) != null) {
                 out.println(line);
             }
         } catch (FileNotFoundException ex) {
-            System.out.println(filenames[0] + " not found");
+            System.out.println(fileToCopy + " not found");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 }
-*/
---
+++ src/CpSingleFile.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * Created by chris on 28/10/2016.
+ */
+public class CpSingleFile {
+    public static void main(String[] args) {
+        new CpSingleFile().launch(args);
+    }
+
+    private void launch(String[] filenames) {
+        File sourceFile = new File(filenames[0]);
+        File destinationFile = new File(filenames[1]);
+        if (destinationFile.exists()) {
+            System.out.print("Overwrite " + destinationFile + " ? (y/n): ");
+            Scanner input = new Scanner(System.in);
+            if (!input.next().equals("y")) {
+                System.out.println("OK - " + destinationFile + " won't be overwritten");
+                return;
+            }
+        }
+        try (BufferedReader in = new BufferedReader(new FileReader(sourceFile));
+             PrintWriter out = new PrintWriter(destinationFile)) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                out.println(line);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(filenames[0] + " not found");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
